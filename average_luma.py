b6af5694ef9c6c9c032338327046016a303ffff6
joseph <moore.joseph@gmail.com>
2019-08-28 12:29:58 -0400

    Adds average_luma.py

---
+++ average_luma.py
@@ -0,0 +1,57 @@
+from PIL import Image, ImageStat
+import PIL
+import glob
+import multiprocessing
+import functools
+
+SRC="/home/delta/Desktop/animals-2019-08-25/153.156.168.63_80/"
+
+# conversion matrix for rgb to gray
+RGB2XYZ = (0.2125, 0.7154, 0.0721, 0,)
+
+#fnames_lumas = []
+
+def set_global_session():
+    global session
+    if not session:
+        session = PIL.Session()
+
+
+def get_fnames(ftype):
+    fnames = [f for f in glob.glob(f"{SRC}*.{ftype}")]
+    return fnames
+
+
+def append_output(alist):
+    def decorator_append(func):
+        @functools.wraps(func)
+        def wrapper_append(*args, **kwargs):
+            value = func(*args, **kwargs)
+            alist.append(value)
+            return value
+        return wrapper_append
+    return decorator_append
+
+
+#@append_output(alist=fnames_lumas)
+def average_luma(imgname):
+    img = Image.open(imgname)
+    if img.mode is 'RGB':
+        img = img.convert('L', RGB2XYZ)
+    return (imgname, ImageStat.Stat(img).mean[0])
+
+
+def get_all_lumas(img_list):
+    with multiprocessing.Pool(processes=4) as pool:
+        fl = pool.map(average_luma, img_list)
+        return fl
+
+
+def main():
+    fnames = get_fnames('jpg')
+    fnames_lumas = get_all_lumas(fnames)
+    return fnames_lumas
+
+
+if __name__ == "__main__":
+    main()
